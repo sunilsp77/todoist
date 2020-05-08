42bcaaf8eed97b61307e8b5df6870d5658057276
Sunil Prakash <sunil.thind17@gmail.com>
2020-05-08 22:37:55 +0530

    Cleaned the index.html file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Header } from "./components/layout/Header";
+import { Content } from "./components/layout/Content";
 
-export const App = ()  =>  (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello there!
-        </p>
-      </header>
-    </div>
-  );
-
+export const App = () => (
+  <div className="App">
+    <Header />
+    <Content />
+  </div>
+);
 
 export default App;
