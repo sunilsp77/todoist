cc97e90d8f267c8de04e4fd1e8b579fff59fd992
Sunil Prakash <sunil.thind17@gmail.com>
2020-05-08 20:57:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const App = ()  =>  (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello there!
+        </p>
+      </header>
+    </div>
+  );
+
+
+export default App;
