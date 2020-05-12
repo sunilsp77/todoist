1e120a73aff9ecfbd1965b70cd0de31ff9dbc11d
SunilPrakash <sunil.prakash@talentica.com>
2020-05-12 12:42:54 +0530

    Update Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { FaPizzaSlice } from "react-icons/fa";
+import PropTypes from "prop-types";
 import { AddTask } from "../AddTask";
 
 export const Header = ({ darkMode, setDarkMode }) => {
   const [shouldShowMain, setShouldShowMain] = useState(false);
   const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
   return (
     <header className="header" data-testid="header">
       <nav>
@@ -13,26 +15,38 @@ export const Header = ({ darkMode, setDarkMode }) => {
         </div>
         <div className="settings">
           <ul>
-            <li
-              data-testid="quick-add-task-action"
-              className="settings__add"
-              onClick={() => {
-                setShowQuickAddTask(true);
-                setShouldShowMain(true);
-              }}
-            >
-              +
+            <li className="settings__add">
+              <button
+                data-testid="quick-add-task-action"
+                aria-label="Quick add task"
+                type="button"
+                onClick={() => {
+                  setShowQuickAddTask(true);
+                  setShouldShowMain(true);
+                }}
+                onKeyDown={() => {
+                  setShowQuickAddTask(true);
+                  setShouldShowMain(true);
+                }}
+              >
+                +
+              </button>
             </li>
-            <li
-              data-testid="dark-mode-action"
-              className="settings__darkmode"
-              onClick={() => setDarkMode(!darkMode)}
-            >
-              <FaPizzaSlice />
+            <li className="settings__darkmode">
+              <button
+                data-testid="dark-mode-action"
+                aria-label="Darkmode on/off"
+                type="button"
+                onClick={() => setDarkMode(!darkMode)}
+                onKeyDown={() => setDarkMode(!darkMode)}
+              >
+                <FaPizzaSlice />
+              </button>
             </li>
           </ul>
         </div>
       </nav>
+
       <AddTask
         showAddTaskMain={false}
         shouldShowMain={shouldShowMain}
@@ -41,4 +55,9 @@ export const Header = ({ darkMode, setDarkMode }) => {
       />
     </header>
   );
+};
+
+Header.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+  setDarkMode: PropTypes.func.isRequired,
 };
