852aaca795437c37d5eb7ef5337551dd003fcd39
SunilPrakash <sunil.prakash@talentica.com>
2020-05-12 13:14:02 +0530

    Update IndividualProject.js

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
+import PropTypes from "prop-types";
 import { useProjectsValue, useSelectedProjectValue } from "../context";
 import { firebase } from "../firebase";
 
@@ -19,6 +20,7 @@ export const IndividualProject = ({ project }) => {
         setSelectedProject("INBOX");
       });
   };
+
   return (
     <>
       <span className="sidebar__dot">•</span>
@@ -58,4 +60,8 @@ export const IndividualProject = ({ project }) => {
       </span>
     </>
   );
+};
+
+IndividualProject.propTypes = {
+  project: PropTypes.object.isRequired,
 };
