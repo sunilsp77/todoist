5d1b9b8cbb39226942b880318310329a1af9d225
SunilPrakash <sunil.prakash@talentica.com>
2020-05-16 17:05:05 +0530

    Updates

---
+++ precache-manifest.851384a70e2d16acbdf2a2b48db29980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55787a4bead60233225ce03ca745de4",
+    "url": "/todoist/index.html"
+  },
+  {
+    "revision": "1cc1f97b5c6b95812505",
+    "url": "/todoist/static/css/main.d5c54971.chunk.css"
+  },
+  {
+    "revision": "622b475dd8631d3ab714",
+    "url": "/todoist/static/js/2.eb2b8f65.chunk.js"
+  },
+  {
+    "revision": "052157e0581e32a2b8fa8f7f1414d257",
+    "url": "/todoist/static/js/2.eb2b8f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc1f97b5c6b95812505",
+    "url": "/todoist/static/js/main.16d3914a.chunk.js"
+  },
+  {
+    "revision": "2ab78a8dc18e4101e7a1",
+    "url": "/todoist/static/js/runtime-main.2c73484e.js"
+  }
+]);
