391c5639338ed4c386ae00468a75957c6a6784e9
Sunil Prakash <sunil.thind17@gmail.com>
2020-05-16 14:46:44 +0530

    Minor corrections in code

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -106,7 +106,9 @@ export const Sidebar = () => {
         <h2>Projects</h2>
       </div>
 
-      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+      <ul className="sidebar__projects">
+        {showProjects && <Projects activeValue={active} />}
+      </ul>
 
       {showProjects && <AddProject />}
     </div>
